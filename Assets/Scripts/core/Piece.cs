546fa33a9e9954a996f1a03d3a63ca289780b4d5
sdercolin <sder.colin@gmail.com>
2020-05-25 04:21:26 +0900

    Implement Piece classes

--- Assets/Scripts/core/Piece.cs
+++ Assets/Scripts/core/Piece.cs
@@ -1,12 +1,11 @@
+using System.Collections.Generic;
+using System.Linq;
+using System;
+
 namespace Core
 {
     public abstract class Piece
     {
-        public Color Color { get; }
-        public abstract PieceType Type { get; }
-
-        public Position Position { get; protected set; }
-
         public Piece(Color color, Position position)
         {
             Color = color;
@@ -18,6 +17,25 @@ namespace Core
             Color = color;
             Position = new Position(color, relativeX, relativeY);
         }
+
+        public Color Color { get; }
+
+        public abstract PieceType Type { get; }
+
+        public Position Position { get; protected set; }
+
+        public abstract List<Position> GetMovablePositions(Game game);
+
+        protected int UpwardFactor => Color == Color.Red ? 1 : -1;
+
+        protected bool IsSameColor(Piece piece)
+        {
+            if (piece == null)
+            {
+                return false;
+            }
+            return piece.Color == Color;
+        }
     }
 
     public class General : Piece
@@ -26,6 +44,14 @@ namespace Core
 
         public General(Color color, int relativeX, int relativeY) : base(color, relativeX, relativeY)
         {
+        }
+
+        public override List<Position> GetMovablePositions(Game game)
+        {
+            return Position.Palaces[Color]
+                .Where(pos => Position.Distance(pos, Position) == 1)
+                .Where(pos => !IsSameColor(game.GetPieceAt(pos)))
+                .ToList();
         }
     }
 
@@ -36,6 +62,14 @@ namespace Core
         public Advisor(Color color, int relativeX, int relativeY) : base(color, relativeX, relativeY)
         {
         }
+
+        public override List<Position> GetMovablePositions(Game game)
+        {
+            return Position.Palaces[Color]
+                .Where(pos => Position.Distance(pos, Position) == 2 && pos.X != Position.X && pos.Y != Position.Y)
+                .Where(pos => !IsSameColor(game.GetPieceAt(pos)))
+                .ToList();
+        }
     }
 
     public class Elephant : Piece
@@ -44,6 +78,23 @@ namespace Core
 
         public Elephant(Color color, int relativeX, int relativeY) : base(color, relativeX, relativeY)
         {
+        }
+
+        public override List<Position> GetMovablePositions(Game game)
+        {
+            return Position.Halves[Color]
+                .Where(pos => Position.Distance(pos, Position) == 4 && pos.X != Position.X && pos.Y != Position.Y)
+                .Where(pos =>
+                {
+                    var route = Position.Route(Position, pos);
+                    if (route.Count != 1)
+                    {
+                        return false;
+                    }
+                    return game.GetPieceAt(route[0]) == null;
+                })
+                .Where(pos => !IsSameColor(game.GetPieceAt(pos)))
+                .ToList();
         }
     }
 
@@ -54,6 +105,15 @@ namespace Core
         public Chariot(Color color, int relativeX, int relativeY) : base(color, relativeX, relativeY)
         {
         }
+
+        public override List<Position> GetMovablePositions(Game game)
+        {
+            return Position.Whole
+                .Where(pos => pos.X == Position.X || pos.Y == Position.Y)
+                .Where(pos => Position.Route(Position, pos).All(routePos => game.GetPieceAt(routePos) == null))
+                .Where(pos => !IsSameColor(game.GetPieceAt(pos)))
+                .ToList();
+        }
     }
 
     public class Horse : Piece
@@ -62,6 +122,37 @@ namespace Core
 
         public Horse(Color color, int relativeX, int relativeY) : base(color, relativeX, relativeY)
         {
+        }
+
+        public override List<Position> GetMovablePositions(Game game)
+        {
+            return Position.Whole
+                .Where(pos => Position.Distance(pos, Position) == 1)
+                .Where(pos => game.GetPieceAt(pos) == null)
+                .SelectMany(middlePos =>
+                {
+                    int diffX = middlePos.X - Position.X;
+                    int diffY = middlePos.Y - Position.Y;
+                    if (diffX != 0)
+                    {
+                        return new List<Position>
+                        {
+                            new Position(middlePos.X + diffX, Position.Y - 1),
+                            new Position(middlePos.X + diffX, Position.Y + 1)
+                        };
+                    }
+                    else
+                    {
+                        return new List<Position>
+                        {
+                            new Position(Position.X - 1, middlePos.Y + diffY),
+                            new Position(Position.X + 1, middlePos.Y + diffY)
+                        };
+                    }
+                })
+                .Where(pos => Position.IsOnTable(pos))
+                .Where(pos => !IsSameColor(game.GetPieceAt(pos)))
+                .ToList();
         }
     }
 
@@ -72,6 +163,26 @@ namespace Core
         public Cannon(Color color, int relativeX, int relativeY) : base(color, relativeX, relativeY)
         {
         }
+
+        public override List<Position> GetMovablePositions(Game game)
+        {
+            var list = new List<Position>();
+            list.AddRange(
+                Position.Whole
+                    .Where(pos => pos.X == Position.X || pos.Y == Position.Y)
+                    .Where(pos => Position.Route(Position, pos).All(routePos => game.GetPieceAt(routePos) == null))
+                    .Where(pos => game.GetPieceAt(pos) == null)
+                    .ToList()
+            );
+            list.AddRange(
+                Position.Whole
+                    .Where(pos => pos.X == Position.X || pos.Y == Position.Y)
+                    .Where(pos => game.GetPieceAt(pos) != null && !IsSameColor(game.GetPieceAt(pos)))
+                    .Where(pos => Position.Route(Position, pos).Count(routePos => game.GetPieceAt(routePos) != null) == 1)
+                    .ToList()
+            );
+            return list;
+        }
     }
 
     public class Soldier : Piece
@@ -80,6 +191,21 @@ namespace Core
 
         public Soldier(Color color, int relativeX, int relativeY) : base(color, relativeX, relativeY)
         {
+        }
+
+        public override List<Position> GetMovablePositions(Game game)
+        {
+            var list = new List<Position>();
+            list.AddRange(
+                new List<Position> { Position.move(0, 1 * UpwardFactor) }.Where(pos => !IsSameColor(game.GetPieceAt(pos)))
+            );
+            if (!Position.Halves[Color].Contains(Position))
+            {
+                list.AddRange(
+                    new List<Position> { Position.move(-1, 0), Position.move(1, 0) }.Where(pos => !IsSameColor(game.GetPieceAt(pos)))
+                );
+            }
+            return list;
         }
     }
 
--- Assets/Scripts/core/Position.cs
+++ Assets/Scripts/core/Position.cs
@@ -1,9 +1,18 @@
+using System.Collections.Generic;
+using System;
+
 namespace Core
 {
     public struct Position
     {
         public int X { get; }
         public int Y { get; }
+
+        public Position(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
 
         public Position(Color color, int relativeX, int relativeY)
         {
@@ -17,6 +26,111 @@ namespace Core
                 X = MAX_X - relativeX;
                 Y = MAX_Y - relativeY;
             }
+        }
+
+        public Position move(int x, int y)
+        {
+            return new Position(this.X + x, this.Y + y);
+        }
+
+        public static int Distance(Position a, Position b)
+        {
+            return Math.Abs(a.X - b.X) + Math.Abs(a.Y - b.Y);
+        }
+
+        public static bool IsOnTable(Position position)
+        {
+            return position.X >= 0 && position.X <= MAX_X && position.Y >= 0 && position.Y <= MAX_Y;
+        }
+
+        public static List<Position> Route(Position a, Position b)
+        {
+            var list = new List<Position>();
+            if (a.X == b.X)
+            {
+                int minY = Math.Min(a.Y, b.Y);
+                int maxY = Math.Max(a.Y, b.Y);
+                for (var y = minY + 1; y < maxY; y++)
+                {
+                    list.Add(new Position(a.X, y));
+                }
+            }
+            else if (a.Y == b.Y)
+            {
+                int minX = Math.Min(a.X, b.X);
+                int maxX = Math.Max(a.X, b.X);
+                for (var x = minX + 1; x < maxX; x++)
+                {
+                    list.Add(new Position(x, a.Y));
+                }
+            }
+            else if (Math.Abs(a.X - b.X) == Math.Abs(a.Y - b.Y))
+            {
+                int minX = Math.Min(a.X, b.X);
+                int maxX = Math.Max(a.X, b.X);
+                int minY = Math.Min(a.Y, b.Y);
+                int yFactor = (a.X - b.X) * (a.Y - b.Y) > 0 ? 1 : -1;
+                for (var diff = 1; diff < maxX - minX; diff++)
+                {
+                    list.Add(new Position(minX + diff, minY + diff * yFactor));
+                }
+            }
+            return list;
+        }
+
+        public static List<Position> Whole = CreateWholeTable();
+
+        static List<Position> CreateWholeTable()
+        {
+            var list = new List<Position>();
+            for (int x = 0; x < Position.MAX_X; x++)
+            {
+                for (int y = 0; y < Position.MAX_Y; y++)
+                {
+                    list.Add(new Position(x, y));
+                }
+            }
+            return list;
+        }
+
+        public static Dictionary<Color, List<Position>> Halves = CreateHalfTables();
+
+        static Dictionary<Color, List<Position>> CreateHalfTables()
+        {
+            var dictionary = new Dictionary<Color, List<Position>>();
+            foreach (var color in new Color[] { Color.Red, Color.Black })
+            {
+                var list = new List<Position>();
+                for (int x = 0; x < MAX_X; x++)
+                {
+                    for (int y = 0; y < 5; y++)
+                    {
+                        list.Add(new Position(color, x, y));
+                    }
+                }
+                dictionary[color] = list;
+            }
+            return dictionary;
+        }
+
+        public static Dictionary<Color, List<Position>> Palaces = CreatePalaces();
+
+        static Dictionary<Color, List<Position>> CreatePalaces()
+        {
+            var dictionary = new Dictionary<Color, List<Position>>();
+            foreach (var color in new Color[] { Color.Red, Color.Black })
+            {
+                var list = new List<Position>();
+                for (int x = 3; x < 6; x++)
+                {
+                    for (int y = 0; y < 3; y++)
+                    {
+                        list.Add(new Position(color, x, y));
+                    }
+                }
+                dictionary[color] = list;
+            }
+            return dictionary;
         }
 
         public const int MAX_X = 8;
